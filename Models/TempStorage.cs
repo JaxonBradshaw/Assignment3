5f48e6147943594e5ad02ff600c0841390aa4ea3
JaxonBradshaw <71052977+JaxonBradshaw@users.noreply.github.com>
2021-02-03 18:01:05 -0700

    Add project files.

---
+++ Models/TempStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class TempStorage
+    {
+        //creating new list
+        private static List<MovieEntry> movies = new List<MovieEntry>();
+
+        //passing info to movies
+        public static IEnumerable<MovieEntry> Movies => movies;
+        //adding whatever moved was passed
+        public static void AddMovie(MovieEntry movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
